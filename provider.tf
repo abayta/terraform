b009f193b5df0db6d1194d6be6f783717c9fc392
Antonio José Gerena Román <ajgerena@intellimentsec.com>
2018-03-16 15:08:28 +0100

    changes

--- provider.tf
+++ provider.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "testAws-public-1" {
     vpc_id = "${aws_vpc.testAws.id}"
     cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-central-1a"
+    availability_zone = "eu-west-3a"
 
     tags {
         Name = "testAws-public-1"
@@ -32,7 +32,7 @@ resource "aws_subnet" "testAws-public-2" {
     vpc_id = "${aws_vpc.testAws.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-central-1b"
+    availability_zone = "eu-west-3b"
 
     tags {
         Name = "testAws-public-2"
@@ -43,7 +43,7 @@ resource "aws_subnet" "testAws-private-1" {
     vpc_id = "${aws_vpc.testAws.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-central-1a"
+    availability_zone = "eu-west-3a"
 
     tags {
         Name = "testAws-private-1"
@@ -53,7 +53,7 @@ resource "aws_subnet" "testAws-private-2" {
     vpc_id = "${aws_vpc.testAws.id}"
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-central-1c"
+    availability_zone = "eu-west-3c"
 
     tags {
         Name = "testAws-private-2"
@@ -84,10 +84,7 @@ resource "aws_route_table" "testAws-public" {
 
 resource "aws_route_table" "testAws-private" {
     vpc_id = "${aws_vpc.testAws.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.testAws-gw.id}"
-    }
+    
     tags {
         Name = "testAws-private-1"
     }
@@ -104,11 +101,11 @@ resource "aws_route_table_association" "testAws-public-2-a" {
 }
 resource "aws_route_table_association" "testAws-private-1-a" {
     subnet_id = "${aws_subnet.testAws-private-1.id}"
-    route_table_id = "${aws_route_table.testAws-private.id}"
+    route_table_id = "${aws_route_table.testAws-public.id}"
 }
 resource "aws_route_table_association" "testAws-private-1-b" {
     subnet_id = "${aws_subnet.testAws-private-2.id}"
-    route_table_id = "${aws_route_table.testAws-private.id}"
+    route_table_id = "${aws_route_table.testAws-public.id}"
 }
 
 # security groups
@@ -144,19 +141,25 @@ tags {
 # instances
 
 resource "aws_instance" "example" {
-  ami           = "ami-c13952ae"
+  ami           = "ami-624bfd1f"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.testAws-private-1.id}"
   vpc_security_group_ids = ["${aws_security_group.webserver.id}"]
   key_name = "${aws_key_pair.mykeypair.key_name}"
+  tags {
+    Name = "webserver-pri"
+  }
 }
 
 resource "aws_instance" "publiweb" {
-  ami           = "ami-c13952ae"
+  ami           = "ami-624bfd1f"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.testAws-public-1.id}"
   vpc_security_group_ids = ["${aws_security_group.webserver.id}"]
   key_name = "${aws_key_pair.mykeypair.key_name}"
+  tags {
+    Name = "webserver-pub"
+  }
 }
 
 # key
